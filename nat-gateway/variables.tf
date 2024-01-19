8efd8cd265c0cff9ddf161ccbfff709cbf589346
pdnt <pdntspa@protonmail.ch>
2024-01-19 12:07:11 -0300

    Fix typo of gateway variable variable "private_data_subnet_az_2id2

--- nat-gateway/variables.tf
+++ nat-gateway/variables.tf
@@ -3,7 +3,7 @@ variable "project_name" {}
 variable "environment" {}
 variable "public_subnet_az1_id" {}
 variable "internet_gateway" {}
-variable "public_subnet_az_id2" {}
+variable "public_subnet_az2_id" {}
 variable "vpc_id" {}
 variable "private_app_subnet_az1_id" {}
 variable "private_data_subnet_az1_id" {}
