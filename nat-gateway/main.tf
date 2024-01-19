f614a4146d212f606945ccfaf0061b2d57fa0c1f
pdnt <pdntspa@protonmail.ch>
2024-01-19 11:04:20 -0300

    Change cidr_block type to string

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -49,7 +49,7 @@ resource "aws_route_table" "private_route_table_az1" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block     = 0.0.0.0/0
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway_az1.id
   }
 
