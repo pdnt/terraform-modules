fbe11ddaba96b3d464cef01e3c1a308e1c415de4
pdnt <pdntspa@protonmail.ch>
2024-01-19 10:25:45 -0300

    Create NAT gateway module

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,11 @@
+#NAT-Gateway variables
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_id" {}
+variable "internet_gateway" {}
+variable "public_subnet_az_id2" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_data_subnet_az2_id" {}
