0c65a50e2c6599f300c6790bd226f595feb57e86
pdnt <pdntspa@protonmail.ch>
2024-01-25 14:37:30 -0300

    Fix public subnet az1 association with the public route table

--- vpc/main.tf
+++ vpc/main.tf
@@ -61,13 +61,13 @@ resource "aws_route_table" "public_route_table" {
 
 # Associate public subnet az1 to "public route table"
 resource "aws_route_table_association" "public_subnet_az1_rt_association" {
-  subnet_id      = aws_subnet.private_app_subnet_az1.id
+  subnet_id      = aws_subnet.public_subnet_az1.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 # Associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_2_rt_association" {
-  subnet_id      = aws_subnet.private_app_subnet_az2.id
+  subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
