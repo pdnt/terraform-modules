1ecc086de3f76e2a3f556c1b8d8d8e4ce54b0150
pdnt <pdntspa@protonmail.ch>
2024-01-26 10:27:19 -0300

    Create aplication load balancer (alb) module

---
+++ alb/variables.tf
@@ -0,0 +1,9 @@
+# Aplication Load Balancer (ABL) variables
+variable "project_name"{}
+variable "environment"{}
+variable "alb_security_group_id"{}
+variable "public_subnet_az1_id"{}
+variable "public_subnet_az2_id"{}
+variable "target_type"{}
+variable "vpc_id"{}
+variable "certificate_arn"{}
