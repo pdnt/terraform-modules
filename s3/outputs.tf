78feb9a2bed943053fd6b5ab3c525694b16b85a3
pdnt <pdntspa@protonmail.ch>
2024-01-26 11:54:19 -0300

    Add s3 module

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,9 +1,9 @@
 # export the s3 bucket name
 output "env_file_bucket_name" {
-  value = 
+  value = var.env_file_bucket_name
 }
 
 # export the environment file name
 output "env_file_name" {
-  value = 
+  value = var.env_file_name
 }
