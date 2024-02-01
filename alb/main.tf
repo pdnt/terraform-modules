1bd851ba4d43432c815fbf324e27da2eb5ea4bb6
pdnt <pdntspa@protonmail.ch>
2024-02-01 11:02:41 -0300

    Modify the target group health_check values for debugging purposes

--- alb/main.tf
+++ alb/main.tf
@@ -21,14 +21,14 @@ resource "aws_lb_target_group" "alb_target_group" {
   vpc_id      = var.vpc_id
 
   health_check {
-    healthy_threshold   = 5
-    interval            = 30
+    healthy_threshold   = 10
+    interval            = 300
     matcher             = "200,301,302"
     path                = "/"
     port                = "traffic-port"
     protocol            = "HTTP"
-    timeout             = 5
-    unhealthy_threshold = 2
+    timeout             = 120
+    unhealthy_threshold = 10
   }
 }
 
