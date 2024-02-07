35cde72dfa4f0857ab2d42832316205147764b6c
pdnt <pdntspa@protonmail.ch>
2024-02-07 20:18:28 -0300

    Configure bucket encryption

--- s3/main.tf
+++ s3/main.tf
@@ -1,9 +1,20 @@
 # create an s3 bucket 
 resource "aws_s3_bucket" "env_file_bucket" {
-  bucket = "${var.project_name}-${var.env_file_bucket_name}"
+  bucket = "${var.project_name}-${var.env_file_bucket_name}-unencrypted"
 
   lifecycle {
     create_before_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = "${var.project_name}-${var.env_file_bucket_name}"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
   }
 }
 
