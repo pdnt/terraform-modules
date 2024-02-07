d7cafa795a4b4e08764040b12df8143b95e382c7
pdnt <pdntspa@protonmail.ch>
2024-02-07 19:59:57 -0300

    Ignore tfsec rule alerting about exposed (public) load balancer

--- alb/main.tf
+++ alb/main.tf
@@ -1,7 +1,7 @@
 # create application load balancer
 resource "aws_lb" "application_load_balancer" {
   name                       = "${var.project_name}-${var.environment}-alb"
-  internal                   = false
+  internal                   = false #tfsec:ignore:aws-elb-alb-not-public
   load_balancer_type         = "application"
   security_groups            = [var.alb_security_group_id]
   subnets                    = [var.public_subnet_az1_id, var.public_subnet_az2_id]
