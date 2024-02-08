b4baf0ebe5030cf2847381754ef57e959691ae7f
pdnt <pdntspa@protonmail.ch>
2024-02-08 14:46:34 -0300

    Ignore tfsec rule alerting that the public subnet is publicly accessible

--- vpc/main.tf
+++ vpc/main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_subnet_az1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_az1_cidr
   availability_zone       = data.aws_availability_zones.available_zones.names[0]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = true #tfsec:ignore:aws-ec2-no-public-ip-subnet
 
   tags = {
     Name = "${var.project_name}-${var.environment}-public-az1"
