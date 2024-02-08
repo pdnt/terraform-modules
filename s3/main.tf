abaf62d3b0eac5037c24470e6c435e85e865f458
pdnt <pdntspa@protonmail.ch>
2024-02-08 13:08:59 -0300

    Rename s3 bucket

--- s3/main.tf
+++ s3/main.tf
@@ -6,7 +6,7 @@ resource "aws_kms_key" "mykey" {
 
 # create an s3 bucket 
 resource "aws_s3_bucket" "env_file_bucket" {
-  bucket = "${var.project_name}-${var.env_file_bucket_name}-unencrypted"
+  bucket = "${var.project_name}-${var.env_file_bucket_name}"
 
   lifecycle {
     create_before_destroy = false
