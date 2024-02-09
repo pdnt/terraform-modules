a3948aeee7370ceea82c790ead34c57cd7eab1bb
pdnt <pdntspa@protonmail.ch>
2024-02-09 10:44:58 -0300

    Add s3 variables

--- s3/variables.tf
+++ s3/variables.tf
@@ -2,3 +2,7 @@
 variable "project_name" {}
 variable "env_file_bucket_name" {}
 variable "env_file_name" {}
+variable "block_public_acls" {}
+variable "block_public_policy" {}
+variable "ignore_public_acls" {}
+variable "restrict_public_buckets" {}
