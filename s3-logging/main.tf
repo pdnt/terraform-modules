cb8eaed545f2a71b60e0b5a4ccae794956ceed86
pdnt <pdntspa@protonmail.ch>
2024-02-19 12:09:38 -0300

    Fix aws_s3_bucket_logging variable name

--- s3-logging/main.tf
+++ s3-logging/main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_acl" "log_bucket_acl" {
 
 # Enable logging
 resource "aws_s3_bucket_logging" "env_file_bucket" {
-  bucket = module.s3.env_file_bucket.id
+  bucket = var.env_file_bucket.id
 
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "log/"
