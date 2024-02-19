2de5bfcdd91856d6e7dd4797d871460e88cc3861
pdnt <pdntspa@protonmail.ch>
2024-02-19 17:11:46 -0300

    Remove ACL from logging bucket module

--- s3-logging/main.tf
+++ s3-logging/main.tf
@@ -3,11 +3,6 @@
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "${var.project_name}-${var.env_file_bucket_name}-logging"
 }
-# Provide ACL to logging bucket
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
-}
 
 # Enable logging
 resource "aws_s3_bucket_logging" "env_file_bucket" {
