35d2009aac1cdfd98f32917a62dc8ac1908198eb
pdnt <pdntspa@protonmail.ch>
2024-02-19 15:53:30 -0300

    Remove env_file_bucket_id variable from variables.tf

--- s3/variables.tf
+++ s3/variables.tf
@@ -2,7 +2,6 @@
 variable "project_name" {}
 variable "env_file_bucket_name" {}
 variable "env_file_name" {}
-variable "env_file_bucket_id" {}
 variable "block_public_acls" {}
 variable "block_public_policy" {}
 variable "ignore_public_acls" {}
