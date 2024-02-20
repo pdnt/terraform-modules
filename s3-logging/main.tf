5a72df1b91d35747b5bfd6bd72449974348028eb
pdnt <pdntspa@protonmail.ch>
2024-02-20 10:09:12 -0300

    Add security policies to logging bucket

--- s3-logging/main.tf
+++ s3-logging/main.tf
@@ -11,3 +11,32 @@ resource "aws_s3_bucket_logging" "env_file_bucket" {
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "log/"
 }
+
+# Encrypt s3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_encryption" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+# Enable blocking any PUT calls with a public ACL
+resource "aws_s3_bucket_public_access_block" "aws_s3_block_PUT_calls" {
+  bucket = aws_s3_bucket.log_bucket.id
+  block_public_acls = var.block_public_acls
+  block_public_policy = var.block_public_policy
+  ignore_public_acls = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
+# Enable versioning to protect against accidental/malicious removal or modification
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
