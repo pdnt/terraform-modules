efebfda83179be875e864f7d5622bfde2ed880b6
pdnt <pdntspa@protonmail.ch>
2024-03-01 12:25:55 -0300

    Delete s3-logging module. Logging is done in s3 module.

--- s3-logging/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "project_name" {}
-variable "env_file_bucket_name" {}
-variable "block_public_acls" {}
-variable "block_public_policy" {}
-variable "ignore_public_acls" {}
-variable "restrict_public_buckets" {}
